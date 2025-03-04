921731dcf6c263f65c3dc4c927001025181fa118
Marco Padilla <marco.padilla.gomez@gmail.com>
2025-03-04 10:58:01 +0100

    feat: Price attributes

--- backend/price/models.py
+++ backend/price/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from backend.item.models import Item
+
+class PriceCategory(enumerate.Enum):
+    SHORT = 'Short'
+    MEDIUM = 'Medium'
+    LONG = 'Long'
+
+class Price(models.Model):
+    item = models.OneToOneField(Item, on_delete=models.CASCADE, related_name="price")
+    price_category = models.CharField(max_length=10, choices=PriceCategory.choices)
+    price1 = models.FloatField()
+    price2 = models.FloatField()
+    price3 = models.FloatField()
+
+    def __str__(self):
+        return self.item.title
