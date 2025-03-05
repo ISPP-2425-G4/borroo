33858dc14aa8e47af42ef41d8e03c5e4f15e10b2
Santi <sanrosray@alum.us.es>
2025-03-05 17:40:19 +0100

    fix: Arreglado el cálculo del total_price y commission

--- backend/rentas/views.py
+++ backend/rentas/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from .models import Rent
 from .serializers import RentSerializer
 
+
 class RentViewSet(viewsets.ModelViewSet):
     queryset = Rent.objects.all()
     serializer_class = RentSerializer
