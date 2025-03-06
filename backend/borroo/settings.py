9d0789349abc022fd49d33bd4f4d986128ee5ff5
Marco Padilla <marco.padilla.gomez@gmail.com>
2025-03-06 13:48:27 +0100

    feat: Pagination added to settings

--- backend/borroo/settings.py
+++ backend/borroo/settings.py
@@ -46,6 +46,12 @@ INSTALLED_APPS = [
     'rentas',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS':
+    'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10  # Se puede cambiar poniendo ?page_size=X en la URL
+}
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
