faec672ae69c7bf087307fdda9ea56508e04330d
Alvaro Martin Muñoz <alvmarmun1@alum.us.es>
2025-03-07 11:01:33 +0100

    feat(frontend): avance prototipo de interfaz inicial

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,11 +1,19 @@
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
+import { Box, display } from "@mui/system";
 
 const Layout = () => {
   return (
     <div>
       <Navbar />
       <main className="p-4">
+        <Box sx={{display: "flex", justifyContent: "center", alignItems: "center", bgcolor:"black", flexDirection:"row"}}>
+          <Box sx={{display: "flex", justifyContent: "center", alignItems: "center", color:"white", bgcolor:"white", width:"200px", height:"200px"}}>
+            Producto 1
+            </Box>
+
+        </Box>
+        
         <Outlet />
       </main>
     </div>
