85e5c7ae76e92016b3c3e52342d04be4da5e4acd
Alvaro Martin Muñoz <alvmarmun1@alum.us.es>
2025-03-07 11:24:35 +0100

    fix: arreglo urls del backend

--- backend/rentas/urls.py
+++ backend/rentas/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import RentViewSet
 
 router = DefaultRouter()
-router.register(r'', RentViewSet)
+router.register(r'full', RentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
