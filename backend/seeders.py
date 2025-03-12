782c5060d5af6340ed3bd922e8b2174d367778ea
frafermot <frafermot@alum.us.es>
2025-03-12 15:15:12 +0100

    feat: seeders de objetos implementado

--- backend/seeders.py
+++ backend/seeders.py
@@ -1,6 +1,8 @@
 import random
 from django.core.management.base import BaseCommand
-from usuarios.models import Usuario
+from usuarios.models import User
+from objetos.models import Item, ItemImage
+from decimal import Decimal
 
 
 class Command(BaseCommand):
@@ -11,7 +13,7 @@ class Command(BaseCommand):
 
     def create_users(self):
         for i in range(10):
-            Usuario.objects.create(
+            User.objects.create(
                 username=f'usuario{i}',
                 name=f'Usuario {i}',
                 surname=f'Apellido {i}',
@@ -26,3 +28,25 @@ class Command(BaseCommand):
                 pricing_plan=random.choice(['free', 'basic', 'premium'])
             )
         self.stdout.write(self.style.SUCCESS('Users created successfully!'))
+
+    def create_items(self):
+        users = User.objects.all()
+        for i in range(10):
+            item = Item.objects.create(
+                title=f'Objeto {i}',
+                description=f'Descripción del objeto {i}',
+                category=random.choice(['technology', 'sports', 'diy',
+                                        'clothing', 'furniture_and_logistics',
+                                        'training']),
+                cancel_type=random.choice(['flexible', 'medium', 'strict']),
+                price_category=random.choice(['hour', 'day', 'week', 'month',
+                                              'year']),
+                price=Decimal(random.uniform(2, 30)),
+                stock=random.randint(1, 100),
+                is_active=bool(random.getrandbits(1)),
+                user=random.choice(users)
+            )
+            ItemImage.objects.create(
+                item=item
+            )
+        self.stdout.write(self.style.SUCCESS('Items created successfully!'))
