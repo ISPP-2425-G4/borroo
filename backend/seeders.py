fea04e1e8c65974bb0f6a1157f195fbf81671136
frafermot <frafermot@alum.us.es>
2025-03-12 14:51:39 +0100

    feat: seeders de usuarios

--- backend/seeders.py
+++ backend/seeders.py
@@ -1,4 +1,6 @@
+import random
 from django.core.management.base import BaseCommand
+from usuarios.models import Usuario
 
 
 class Command(BaseCommand):
@@ -6,3 +8,21 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Seed completed successfully!'))
+
+    def create_users(self):
+        for i in range(10):
+            Usuario.objects.create(
+                username=f'usuario{i}',
+                name=f'Usuario {i}',
+                surname=f'Apellido {i}',
+                email=f'usuario{i}@example.com',
+                password='contraseña123',
+                phone_number=f'+123456789{i}',
+                country='España',
+                city='Madrid',
+                address=f'Calle {i}',
+                postal_code='28001',
+                is_verified=bool(random.getrandbits(1)),
+                pricing_plan=random.choice(['free', 'basic', 'premium'])
+            )
+        self.stdout.write(self.style.SUCCESS('Users created successfully!'))
