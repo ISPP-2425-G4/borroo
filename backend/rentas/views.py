30d504f5dcf7cd7aa5d3306978b518f71f2482a6
juliasxez <julsanmar2@alum.us.es>
2025-03-13 18:07:17 +0100

    fix: añadir validaciones a rent

--- backend/rentas/views.py
+++ backend/rentas/views.py
@@ -2,17 +2,21 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status, filters
-from .models import Rent, RentStatus
+from .models import Rent, RentStatus, Item
 from .serializers import RentSerializer
-from rest_framework.exceptions import NotAuthenticated, PermissionDenied, NotFound
+from rest_framework.exceptions import NotAuthenticated, PermissionDenied
+from rest_framework.exceptions import NotFound
 from django.contrib.auth.models import AnonymousUser
+from django.shortcuts import get_object_or_404
 
 
 def is_authorized(condition=True, authenticated=True):
     if not authenticated:
-        raise NotAuthenticated({'error': 'Debes estar autenticado.'})
+        raise NotAuthenticated(
+            {'error': 'Debes estar autenticado.'})
     elif not condition:
-        raise PermissionDenied({'error': 'No tienes permisos para realizar esta acción.'})
+        raise PermissionDenied(
+            {'error': 'No tienes permisos para realizar esta acción.'})
 
 
 class RentViewSet(viewsets.ModelViewSet):
@@ -38,11 +42,22 @@ class RentViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['post'])
     def first_request(self, request, *args, **kwargs):
-        objeto_id = self.kwargs.get('id')
+        # el frontend pasa la informacion necesaria en el body
+        item_id = request.data.get('item')
         start_date = request.data.get('start_date')
         end_date = request.data.get('end_date')
-        is_authorized(authenticated=self.request.user.is_authenticated)
-        if Rent.objects.filter(item_id=objeto_id, start_date__lte=end_date,
+        user = self.request.user if not AnonymousUser else None
+        authenticated = self.request.user.is_authenticated
+
+        item = get_object_or_404(Item, pk=item_id)
+
+        not_rent_yourself = user != item.user
+
+        is_authorized(condition=not_rent_yourself, authenticated=authenticated)
+
+        if Rent.objects.filter(item=item, start_date__lte=end_date,
+                               # por dentro django usa la pk de item
+                               # para el filtro
                                end_date__gte=start_date).exists():
             return Response(
                 {'error': 'El objeto no está disponible en esas fechas'},
@@ -50,7 +65,7 @@ class RentViewSet(viewsets.ModelViewSet):
 
         serializer = RentSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(renter=request.user, item=objeto_id)
+            serializer.save(renter=user, item=item)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -59,53 +74,23 @@ class RentViewSet(viewsets.ModelViewSet):
         rent = self.get_object()
         response = request.data.get("response")
 
-        if request.user == rent.item.user:
-            return self._handle_owner_action(rent, response)
-        elif request.user == rent.renter:
-            return self._handle_renter_action(rent, response)
+        user = self.request.user if not AnonymousUser else None
+        authenticated = self.request.user.is_authenticated
+        is_owner = user == rent.item.user
 
-        return Response(
-            {'error': 'No tienes permiso para gestionar este alquiler.'},
-            status=status.HTTP_403_FORBIDDEN
-        )
-
-    def _handle_owner_action(self, rent, response):
+        is_authorized(condition=is_owner, authenticated=authenticated)
         if response == "accepted":
             rent.rent_status = RentStatus.BOOKED
             rent.save()
             return Response({'status': 'Solicitud aceptada. '
                             'El objeto ha sido reservado.'})
-
         elif response == "rejected":
             rent.rent_status = RentStatus.CANCELLED
             rent.save()
             return Response({'status': 'Solicitud rechazada. '
                             'El alquiler se ha cancelado.'})
-
-        elif response == "picked_up":
-            if rent.rent_status == RentStatus.BOOKED:
-                rent.rent_status = RentStatus.PICKED_UP
-                rent.save()
-                return Response(
-                    {'status': 'El objeto ha sido entregado al arrendatario.'})
-            return Response(
-                {'error': 'Solo puedes cambiar a "PICKED_UP" si está '
-                 'en estado "BOOKED".'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        return Response({'error': 'No puedes realizar esta acción.'},
-                        status=status.HTTP_400_BAD_REQUEST)
-
-    def _handle_renter_action(self, rent, response):
-        if response == "returned" and rent.rent_status == RentStatus.PICKED_UP:
-            rent.rent_status = RentStatus.RETURNED
-            rent.save()
-            return Response({'status':
-                            'El objeto ha sido devuelto correctamente.'})
-
-        return Response({'error': 'No puedes realizar esta acción.'},
-                        status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'No existe un response adecuado'})
 
     @action(detail=True, methods=['put'])
     def cancel_rent(self, request, pk=None):
