11deca7f6c62eb18724976b4b35a040019822c9d
juliasxez <julsanmar2@alum.us.es>
2025-03-19 23:05:33 +0100

    feat: añadir validaciones en fechas para solicitar un objeto

--- backend/rentas/serializers.py
+++ backend/rentas/serializers.py
@@ -1,5 +1,7 @@
+from datetime import timedelta
 from rest_framework import serializers
 from .models import Rent
+import calendar
 
 # Sirve para validar los datos que llegan del formulario y mapearlos al modelo
 
@@ -12,6 +14,7 @@ class RentSerializer(serializers.ModelSerializer):
                             'payment_status', 'renter')
 
     def validate(self, data):
+        item = data.get('item')
         start_date = data.get("start_date")
         end_date = data.get("end_date")
 
@@ -20,4 +23,39 @@ class RentSerializer(serializers.ModelSerializer):
                 {"end_date": "La fecha de fin debe ser posterior '"
                     'a la fecha de inicio."'}
             )
+
+        year = end_date.year
+        month = end_date.month
+
+        if item:
+            price_category = item.price_category
+            if price_category == "hour":
+                expected_duration = timedelta(hours=23)
+                actual_duration = end_date - start_date
+                if actual_duration > expected_duration:
+                    raise serializers.ValidationError({
+                        "end_date": "El intervalo para alquiler por hora no "
+                        "puede superar las 23 horas."
+                    })
+            elif price_category == "week":
+                expected_duration = timedelta(days=7)
+                actual_duration = end_date - start_date
+                if (actual_duration < expected_duration
+                        or actual_duration > expected_duration):
+                    raise serializers.ValidationError({
+                        "end_date": "El rango debe abarcar 7 días para "
+                        "alquiler semanal."
+                    })
+            elif price_category == "month":
+                if start_date.day != 1:
+                    raise serializers.ValidationError({
+                        "start_date": "Para alquiler mensual, la fecha de "
+                        "inicio debe ser el primer día del mes."
+                    })
+                first_weekday, num_days = calendar.monthrange(year, month)
+                if end_date.day != num_days:
+                    raise serializers.ValidationError({
+                        "end_date": "La fecha de fin debe ser el último "
+                        "día del mes."
+                    })
         return data
