b126ea868692df043bae98eba371b1d35690df7d
Alexander Picón Garrote <alepingar@alum.us.es>
2025-03-19 12:33:12 +0100

    feat: añadir metodo con campos de rent

--- backend/pagos/views.py
+++ backend/pagos/views.py
@@ -1,33 +1,36 @@
 import stripe
 from django.conf import settings
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.views import View
+from rentas.models import Rent
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
-def checkout(request):
-    return render(request, "checkout.html", {"stripe_public_key": settings.STRIPE_PUBLIC_KEY})
+class CreateCheckoutSessionView(View):
+    def post(self, request, *args, **kwargs):
+        rent_id = request.data.get('rent_id')
+        rent = Rent.objects.get(id=rent_id)
 
-def create_checkout_session(request):
-    try:
+        # Crear la sesión de Stripe
         session = stripe.checkout.Session.create(
-            payment_method_types=["card"],
+            payment_method_types=['card'],
             line_items=[
                 {
-                    "price_data": {
-                        "currency": "eur",
-                        "product_data": {
-                            "name": "Realiar el pago",
+                    'price_data': {
+                        'currency': 'eur',
+                        'product_data': {
+                            'name': f"Alquiler de {rent.item.title}",
                         },
-                        "unit_amount": 1000,
+                        'unit_amount': int(rent.total_price * 100),
                     },
-                    "quantity": 1,
-                }
+                    'quantity': 1,
+                },
             ],
-            mode="payment",
-            success_url="http://127.0.0.1:8000/success/",
-            cancel_url="http://127.0.0.1:8000/cancel/",
+            mode='payment',
+            success_url=request.build_absolute_uri('/success/'),
+            cancel_url=request.build_absolute_uri('/cancel/'),
         )
-        return JsonResponse({"id": session.id})
-    except Exception as e:
-        return JsonResponse({"error": str(e)}, status=400)
+
+        return JsonResponse({
+            'id': session.id
+        })
