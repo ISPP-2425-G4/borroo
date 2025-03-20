613288b9b52668bafdfef66e1215cf5c67a07c49
Santi <sanrosray@alum.us.es>
2025-03-20 10:22:35 +0100

    feat: Añadido is_featured en models de los objetos

--- backend/objetos/models.py
+++ backend/objetos/models.py
@@ -54,6 +54,7 @@ class Item(models.Model):
     user = models.ForeignKey('usuarios.User', related_name='items',
                              on_delete=models.CASCADE)
     draft_mode = models.BooleanField(default=False)
+    is_featured = models.BooleanField(default=False)
 
 
 class ItemImage(models.Model):
