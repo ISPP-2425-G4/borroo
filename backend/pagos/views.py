c04ecbc5d5d0b30518cef4abc2fd5f7889c3fd1e
Alvaro Martin Muñoz <alvmarmun1@alum.us.es>
2025-03-25 17:50:27 +0100

    feat: implementacion backend para los cambios de subscripciones

--- backend/pagos/views.py
+++ backend/pagos/views.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rentas.models import Rent, PaymentStatus
+from usuarios.models import User
+import json
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -76,3 +78,72 @@ def confirmar_pago(request, session_id):
 
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=400)
+    
+@csrf_exempt
+def create_subscription_checkout(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            user = User.objects.get(id=data.get('user_id'))
+            currency = data.get('currency', 'EUR')
+            amount = data.get('price')*100
+
+            session = stripe.checkout.Session.create(
+                payment_method_types=['card'],
+                line_items=[{
+                    'price_data': {
+                        'currency': currency,
+                        'product_data': {
+                            'name': 'Subscripcion mensual',
+                        },
+                        'unit_amount': amount,
+                    },
+                    'quantity': 1,
+                }],
+                mode='payment',
+                success_url="http://localhost:5173/pricing-plan?session_id={CHECKOUT_SESSION_ID}",
+                cancel_url="http://localhost:5173/pricing-plan",
+                metadata={
+                    'user_id': user.id
+                }
+            )
+            return JsonResponse({'id': session.id})
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+    return JsonResponse({'error': 'Método no permitido'}, status=405)
+
+
+@csrf_exempt
+def confirm_subscription_checkout(request, session_id):
+    try:
+        session = stripe.checkout.Session.retrieve(session_id)
+
+        if session.payment_status == 'paid':
+            metadata = session.metadata
+            print("metadata",metadata)
+            user_id = metadata.get('user_id')
+
+            if user_id:
+                try:
+                    user = User.objects.get(id=user_id)
+                    user.pricing_plan = "premium"
+                    user.stripe_customer_id = session.customer
+                    user.stripe_subscription_id = session.subscription
+                    user.save()
+
+                    return JsonResponse({
+                        'status': 'success',
+                        'user_id': user.id,
+                        'plan': user.pricing_plan
+                    })
+                except User.DoesNotExist:
+                    return JsonResponse({'error': 'Usuario no encontrado'},
+                                        status=404)
+            else:
+                return JsonResponse({
+                    'error': 'user_id no encontrado en metadatos'}, status=400)
+        else:
+            return JsonResponse({'status': 'unpaid'}, status=402)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
+
