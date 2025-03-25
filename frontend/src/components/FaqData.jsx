4af771aa7dab7c85f1f54cb9c654670034dddcd9
Marco Padilla <marco.padilla.gomez@gmail.com>
2025-03-25 14:35:32 +0100

    feat: Añadida la pantalla de FAQ

---
+++ frontend/src/components/FaqData.jsx
@@ -0,0 +1,32 @@
+const faqData = [
+    {
+      question: '¿Cómo puedo realizar un pedido?',
+      answer: (
+        <>
+          Puedes navegar por nuestro <strong>catálogo</strong>, añadir los productos al carrito y completar la compra desde la sección <em>"Finalizar pedido"</em>.
+        </>
+      )
+    },
+    {
+      question: '¿Qué métodos de pago aceptáis?',
+      answer: (
+        <ul>
+          <li>Tarjeta de crédito/débito</li>
+          <li>PayPal</li>
+          <li>Transferencia bancaria</li>
+        </ul>
+      )
+    },
+    {
+        question: '¿Puedo darme de baja de mi suscripción premium?',
+        answer: (
+          <>
+            Aunque nos de mucha pena, si, eres libre de cancelar tu suscripción cuando desees.
+            <br />
+            Solo tendrás que ir al apartado de <strong>Plan de suscripción</strong> y cambiarte al plan gratuito.
+          </>
+        )
+      },
+  ];
+  
+  export default faqData;
