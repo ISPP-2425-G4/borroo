dfdce450eee89a7c31b8ae0f3f54ca5866931d02
pabdiaord <pabdiaord@alum.us.es>
2025-03-26 17:13:50 +0100

    fix: Solventado error al resolver conflictos

--- backend/objetos/views.py
+++ backend/objetos/views.py
@@ -269,6 +269,14 @@ class ItemViewSet(viewsets.ModelViewSet):
             {"message": "El objeto ahora es destacado."},
             status=status.HTTP_200_OK
         )
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        featured = self.request.query_params.get('featured', None)
+        if featured is not None and featured.lower() == 'true':
+            queryset = queryset.filter(featured=True)
+
+        return queryset
 
 
 class ItemImageViewSet(viewsets.ModelViewSet):
