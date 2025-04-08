a6628516c724c50f1e1ca006180adefe6a36a1b2
juliasxez <julsanmar2@alum.us.es>
2025-04-08 17:00:36 +0200

    feat(backend): añadir update y destroy ticket view

--- backend/tickets/views.py
+++ backend/tickets/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from .models import Ticket, User
-from rest_framework import status, filters
+from .models import Ticket, TicketStatus
+from rest_framework import status, filters, viewsets
 from rest_framework.response import Response
+from django.db.models import Q
+from django.utils.timezone import now
+
 
 # Create your views here.
 class TicketViewSet(viewsets.ModelViewSet):
@@ -14,16 +16,21 @@ class TicketViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
+        if not self.request.user.is_authenticated:
+            return Response(
+                {"detail": "Authentication credentials were not provided."},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
         # Se muestran los tickets en los que el usuario es:
         # - Quien reporta la incidencia (reporter)
         # - Quien la gestiona (manager)
         # - O el propietario del objeto (a través de la renta)
         return Ticket.objects.filter(
-            Q(reporter=user) |
+            Q(reporter=user) |  # OR
             Q(manager=user) |
             Q(rent__item__user=user)
         ).distinct()
-    
+
     def create(self, request, *args, **kwargs):
 
         if not request.user.is_authenticated:
@@ -38,6 +45,60 @@ class TicketViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
 
-        ticket = serializer.save()
+        serializer.save()
         return Response(
             serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        if not request.user.is_authenticated and not request.user.is_admin:
+            return Response(
+                {"detail": "Authentication credentials were not provided."},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        instance = self.get_object()
+        data = request.data.copy()
+
+        if data.get("status") in [TicketStatus.RESOLVED, 
+                                  TicketStatus.CANCELLED]:
+            data["closed_at"] = now()
+        else:
+            allowed_fields = ['description']
+            data = {key: value for key, value in data.items()
+                    if key in allowed_fields}
+
+        serializer = self.get_serializer(
+            instance, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        # Verificamos que el usuario esté autenticado
+        if not request.user.is_authenticated:
+            return Response(
+                {"detail": "Authentication credentials were not provided."},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        # Recuperamos el objeto a eliminar
+        try:
+            ticket = self.get_object()
+        except Ticket.DoesNotExist:
+            return Response({"detail": "Incidencia no encontrada."},
+                            status=status.HTTP_404_NOT_FOUND)
+
+        if not request.user.is_admin:
+            return Response(
+                {"detail": "No tienes permiso para eliminar esta incidencia."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        # Eliminamos el objeto
+        ticket.delete()
+
+        return Response(
+            {"detail": "Incidencia eliminada exitosamente."},
+            status=status.HTTP_204_NO_CONTENT
+        )
